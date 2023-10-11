184590e7918e18018187c08a85b0be0ec2cdccc3
Kurniawan Cristianto <kurniawancristianto@gmail.com>
2023-10-11 14:04:53 +0700

    add loading component

---
+++ resources/js/components/Loading.vue
@@ -0,0 +1,32 @@
+<template>
+    <div
+        class="loader w-[80px] md:w-[120px] aspect-square border-[12px] md:border-[16px] border-t-green-600 border-neutral-200"
+    ></div>
+</template>
+
+<style scoped>
+.loader {
+    border-radius: 50%;
+    -webkit-animation: spin 1s linear infinite; /* Safari */
+    animation: spin 1s linear infinite;
+}
+
+/* Safari */
+@-webkit-keyframes spin {
+    0% {
+        -webkit-transform: rotate(0deg);
+    }
+    100% {
+        -webkit-transform: rotate(360deg);
+    }
+}
+
+@keyframes spin {
+    0% {
+        transform: rotate(0deg);
+    }
+    100% {
+        transform: rotate(360deg);
+    }
+}
+</style>
--- resources/js/pages/HomePage.vue
+++ resources/js/pages/HomePage.vue
@@ -1,7 +1,7 @@
 <template>
-    <main class="flex-1 flex flex-col py-8 gap-16 items-center">
-        <h1 class="text-4xl font-bold">Sriflix</h1>
-        <p v-show="getMovies.isFetching.value">Fetching...</p>
+    <main class="flex-1 flex flex-col gap-16 justify-center items-center">
+        <h1 class="text-4xl font-bold absolute top-4">Sriflix</h1>
+        <loading v-show="getMovies.isFetching.value" />
         <ul
             v-show="!getMovies.isFetching.value"
             class="flex flex-wrap justify-center items-stretch gap-4 text-center w-full md:w-2/3"
@@ -15,6 +15,7 @@
 import { useGetMovies } from "../composables/Movie";
 import { onMounted } from "vue";
 import MovieCard from "../components/MovieCard.vue";
+import Loading from "../components/Loading.vue";
 
 const getMovies = useGetMovies();
 
