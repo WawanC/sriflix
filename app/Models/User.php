8d703c09a73366cb0791169cecdbd481b18b7e93
Kurniawan Cristianto <kurniawancristianto@gmail.com>
2023-10-12 11:44:44 +0700

    create user model & migration

---
+++ app/Models/User.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Model;
+
+class User extends Model
+{
+    use HasUuids;
+
+    protected $fillable = ["username", "password"];
+}
