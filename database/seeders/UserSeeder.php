cf32673fd8c5735ed7efb22f3dc27c42f724627e
Kurniawan Cristianto <kurniawancristianto@gmail.com>
2023-10-12 20:54:03 +0700

    create users db seeder

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::create([
+            "username" => "admin123",
+            "password" => Hash::make("123456")
+        ]);
+    }
+}
