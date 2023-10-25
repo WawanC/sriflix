2b983ab3b18a093189ee9bb0f9ab7abed76f8933
Kurniawan Cristianto <kurniawancristianto@gmail.com>
2023-10-25 10:42:17 +0700

    add image lazy loading on movie card

--- resources/js/components/MovieCard.vue
+++ resources/js/components/MovieCard.vue
@@ -1,26 +1,29 @@
 <template>
-    <router-link
-        :key="props.movie.id"
-        :to="`/movies/${props.movie.id}`"
-        class="w-[100px] md:w-[200px] h-[150px] md:h-[300px] flex relative rounded overflow-hidden shadow"
-    >
-        <img
-            ref="imageRef"
-            :alt="props.movie.id"
-            :src="props.movie.picture_url"
-            class="w-full h-full object-cover hidden"
-        />
-        <div v-if="!isImageLoaded" class="w-full h-full bg-neutral-300"></div>
-        <div class="absolute bottom-0 w-full h-1/4">
-            <div class="bg-black w-full h-full opacity-75" />
+    <router-link :to="`/movies/${props.movie.id}`">
+        <article
+            ref="cardRef"
+            class="w-[100px] md:w-[200px] h-[150px] md:h-[300px] flex relative rounded overflow-hidden shadow"
+        >
+            <img
+                :alt="props.movie.id"
+                class="w-full h-full object-cover hidden card_image"
+            />
             <div
-                class="absolute inset-0 text-white flex flex-col justify-center items-center"
-            >
-                <span class="font-semibold text-xs md:text-base line-clamp-2">{{
-                    props.movie.title
-                }}</span>
+                v-if="!isImageLoaded"
+                class="w-full h-full bg-neutral-300"
+            ></div>
+            <div class="absolute bottom-0 w-full h-1/4">
+                <div class="bg-black w-full h-full opacity-75" />
+                <div
+                    class="absolute inset-0 text-white flex flex-col justify-center items-center"
+                >
+                    <span
+                        class="font-semibold text-xs md:text-base line-clamp-2"
+                        >{{ props.movie.title }}</span
+                    >
+                </div>
             </div>
-        </div>
+        </article>
     </router-link>
 </template>
 
@@ -33,15 +36,31 @@ const props = defineProps<{
 }>();
 
 const isImageLoaded = ref(false);
-const imageRef = ref<HTMLImageElement | null>(null);
+const cardRef = ref<HTMLDivElement | null>(null);
+const observer = ref<IntersectionObserver>(
+    new IntersectionObserver((entries, obs) => {
+        entries.forEach((entry) => {
+            if (!entry.isIntersecting) return;
+            console.log("intersecting...");
+
+            const targetImage = entry.target.querySelector(
+                ".card_image",
+            ) as HTMLImageElement;
+            if (!targetImage) return;
+
+            targetImage.onload = () => {
+                isImageLoaded.value = true;
+                targetImage.style.display = "block";
+            };
+            targetImage.src = props.movie.picture_url;
+
+            obs.unobserve(entry.target);
+        });
+    }),
+);
 
 onMounted(() => {
-    if (!imageRef.value) return;
-    imageRef.value.onload = () => {
-        if (!imageRef.value) return;
-        isImageLoaded.value = true;
-        imageRef.value.style.display = "block";
-    };
-    imageRef.value.src = props.movie.picture_url;
+    if (!cardRef.value) return;
+    observer.value.observe(cardRef.value);
 });
 </script>
