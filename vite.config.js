198dc53c55199c294b23354defa21098c31dcc24
Kurniawan Cristianto <kurniawancristianto@gmail.com>
2023-11-04 13:28:56 +0700

    fix vite build minify error

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import laravel from "laravel-vite-plugin";
 import vuePlugin from "@vitejs/plugin-vue";
 
 export default defineConfig({
+    build: {
+        minify: false,
+    },
     plugins: [
         laravel({
             input: [
